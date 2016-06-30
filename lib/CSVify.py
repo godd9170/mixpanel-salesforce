31ab51e7e5a52d23308bc9e2c962aa95dd7e8fb5
Henry Goddard <godd9170@mylaurier.ca>
2016-06-30 11:47:03 -0400

    write csv to s3 bucket

--- lib/CSVify.py
+++ lib/CSVify.py
@@ -1,17 +1,15 @@
 import time
+import boto3
 import csv
-import os
 
 class CSVify(object):
 	def __init__(self, filename, header):
-		cwd = os.getcwd()
-		if not os.path.isdir(cwd + '/' + time.strftime("%d%m%Y")):
-			os.mkdir(time.strftime("%d%m%Y"))
-		self.filename = time.strftime("%d%m%Y") + '/' + filename
+		self.filename = time.strftime("%d%m%Y") + filename
 		self.header = header
+		self.csvfile = None
 
 	def write(self, rows):
-		with open(self.filename, 'wb') as csvfile:
+		with open('/tmp/' + self.filename, 'wb') as csvfile:
 
 			writer = csv.writer(csvfile, delimiter=',')
 			writer.writerow(self.header)
@@ -23,3 +21,12 @@ class CSVify(object):
 					writer.writerow(cells)
 				except:
 					print("----------- BAD LINE -----------")
+		self.uploadS3()
+
+	def uploadS3(self):
+		if self.filename is not None:
+			s3_client = boto3.client('s3')
+			print "Uploading %s to s3" % self.filename
+			s3_client.upload_file('tmp/' + self.filename, '<S3 Bucket>', time.strftime("%d%m%Y") +'/' + self.filename)
+		else:
+			print "Please Write A CSV File First"
